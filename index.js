454e2678eb0d06e93ccaa320b18384c9c13e0753
assemtolen <43029664+assemtolen@users.noreply.github.com>
2018-12-23 16:57:23 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function f() {
     document.getElementById("menu").classList.toggle("show");
 }
 
@@ -9,4 +9,23 @@ window.onclick = function(e) {
         menu.classList.remove('show');
       }
   }
+}
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000);  
 }
